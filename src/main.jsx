96afbf05735b3971913216ad7eeab4af68152781
ArefinShaon <arefinshaon99@gmail.com>
2023-05-02 10:54:44 +0600

    log in page added

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './components/home/Home.jsx';
-import Main from './components/Layout/Main.jsx';
-import Blog from './components/Blog/Blog.jsx';
-import Login from './components/Login/Login.jsx';
-import Recipes from './components/Recipes/Recipes.jsx';
-import AboutInfo from './components/About infomation/AboutInfo.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/home/Home.jsx";
+import Main from "./components/Layout/Main.jsx";
+import Blog from "./components/Blog/Blog.jsx";
+import Login from "./components/Login/Login.jsx";
+import Recipes from "./components/Recipes/Recipes.jsx";
+import AboutInfo from "./components/About infomation/AboutInfo.jsx";
+import AuthProvide from "./components/contexts/AuthProvide.jsx";
 
 const router = createBrowserRouter([
   {
@@ -19,32 +17,34 @@ const router = createBrowserRouter([
     element: <Main></Main>,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home></Home>,
-        loader: () => fetch(`http://localhost:5000/chefs`)
+        loader: () => fetch(`http://localhost:5000/chefs`),
       },
       {
-        path: '/blog',
-        element:<Blog></Blog>
+        path: "/blog",
+        element: <Blog></Blog>,
       },
       {
-        path: '/login',
-        element:<Login></Login>
+        path: "/login",
+        element: <Login></Login>,
       },
       {
-        path: '/recipe',
-        element:<Recipes></Recipes>
+        path: "/recipe",
+        element: <Recipes></Recipes>,
       },
       {
-        path: '/about',
-        element:<AboutInfo></AboutInfo>
-      }
-    ]
+        path: "/about",
+        element: <AboutInfo></AboutInfo>,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <AuthProvide>
+      <RouterProvider router={router} />
+    </AuthProvide>
+  </React.StrictMode>
+);
